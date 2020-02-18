e53adf3fa6d36ab4f74410d8f8bfe19e9496befa
jsanter27 <jsanter@cs.stonybrook.edu>
2020-02-18 10:07:10 -0500

    First query successful

--- mydig.py
+++ mydig.py
@@ -11,16 +11,14 @@ import sys
 
 def main(argc, argv):
 
-    # Checks to see if the arguments are in a valid form
+    # Sets the domain name accordingly
     if argc == 1:
-        print("No argument given")
-        return
+        domain_name = input("Enter a domain: ")
     elif argc > 2:
         print("Invalid number of arguments given")
         return
-
-    # Set the domain to be resolved
-    domain_name = argv[1]
+    else:
+        domain_name = argv[1]
 
     # Calls mydig function starting with root server
     mydig(domain_name, "198.41.0.4")
@@ -32,7 +30,7 @@ def mydig(domain_name, ip_address):
     """Recursive DNS resolver function"""
 
     message = dns.message.make_query(domain_name, "A")
-    response = dns.query.udp(message, ip_address, timeout=15)
+    response = dns.query.udp(message, ip_address, timeout=10)
     print(response.to_text())
 
     return
