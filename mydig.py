1220990106e662a9e86c2a99ab48bd88490728b8
jsanter27 <jsanter@cs.stonybrook.edu>
2020-02-17 21:47:10 -0500

    TODO: find out why send_udp gets stuck

--- mydig.py
+++ mydig.py
@@ -5,15 +5,41 @@
 # February 24, 2020
 
 import dns.query
+import dns.message
+import sys
 
 
-def main():
-    domain_name = input("Enter a domain name: ")
+def main(argc, argv):
+
+    # Checks to see if the arguments are in a valid form
+    if argc == 1:
+        print("No argument given")
+        return
+    elif argc > 2:
+        print("Invalid number of arguments given")
+        return
+
+    # Set the domain to be resolved
+    domain_name = argv[1]
+
+    # Calls mydig function starting with root server
+    mydig(domain_name, "198.41.0.4")
+
+    return
+
+
+def mydig(domain_name, ip_address):
+    """Recursive DNS resolver function"""
+
+    message = dns.message.make_query(domain_name, "A")
+    response = dns.query.udp(message, ip_address, timeout=15)
+    print(response.to_text())
+
     return
 
 
 if __name__ == "__main__":
-    main()
+    main(len(sys.argv), sys.argv)
 
 
 
