b4b3b8ae6a11776e71af893fc41e61fcf24792a5
jsanter27 <jsanter@cs.stonybrook.edu>
2020-02-19 14:39:54 -0500

    DNS Resolver functional along with bonus CNAME resolver

--- mydig.py
+++ mydig.py
@@ -9,6 +9,8 @@ import dns.message
 import dns.name
 import sys
 
+global_cname = []  # Keeps track of CNAMEs
+
 
 def main(argc: int, argv: list):
 
@@ -23,36 +25,79 @@ def main(argc: int, argv: list):
 
     domain_list = domain_name.split(".")
     domain_list = [x + "." for x in domain_list]
+    domain_name = domain_name + "."
 
-    # Calls mydig function starting with root server
-    response = mydig("198.41.0.4", domain_list, len(domain_list), domain_name)
+    # Calls mydig function starting with chosen root server
+    answer = mydig("198.41.0.4", domain_name, len(domain_list)-1)
+
+    # Prints result
+    print("QUESTION SECTION:")
+    print(domain_name + " IN A")
+    print("ANSWER SECTION:")
+    for i in global_cname:
+        print(i)
+    print(answer)
+    print()
+    print("Query time: ")
+    print("WHEN: ")
 
     return
 
 
-def mydig(ip_address: str, domain_list: list, domain_size: int, domain_name: str):
+def mydig(ip_address: str, domain_name: str, domain_index: int):
     """Recursive DNS resolver function"""
-
     # Make a Message object
     message = dns.message.make_query(domain_name, "A")
 
     # Send the Message as a query and save the response
     response = dns.query.udp(message, ip_address, timeout=10)
-    print(response.to_text())
 
-    # Find the RRset
-    rrset = response.get_rrset(dns.message.AUTHORITY, dns.name.from_text(domain_list[domain_size-1]),
-                               dns.rdataclass.IN, dns.rdatatype.NS)
-    if rrset is None:
-        print("Error: query failed")
+    # First check if Answer RRset has answer A
+    answer = response.get_rrset(dns.message.ANSWER, dns.name.from_text(domain_name), dns.rdataclass.IN, dns.rdatatype.A)
+    if answer is not None:
+        return answer
+
+    # BONUS: check if Answer RRset has answer CNAME and resolve it
+    cname = response.get_rrset(dns.message.ANSWER, dns.name.from_text(domain_name),
+                               dns.rdataclass.IN, dns.rdatatype.CNAME)
+    if cname is not None:
+        global_cname.append(cname)
+        cname_list = cname[0].to_text().split(".")
+        cname_list = cname_list[:-1]
+        return mydig("198.41.0.4", cname[0].to_text(), len(cname_list)-1)
+
+    # Find the Authority RRset
+    authority = response.get_rrset(dns.message.AUTHORITY, dns.name.from_text(parse_domain(domain_name, domain_index)),
+                                   dns.rdataclass.IN, dns.rdatatype.NS)
+    if authority is None:
+        print("Error: no AUTHORITY section found")
         return None
 
-    print()
-    print(rrset.to_text())
-    print()
-    print(rrset[0])
+    # Find the Additional RRset
+    additional = response.get_rrset(dns.message.ADDITIONAL, dns.name.from_text(authority[0].to_text()),
+                                    dns.rdataclass.IN, dns.rdatatype.A)
+    if additional is None:
+        print("Error: no ADDITONAL section found")
+        return None
 
-    return response
+    # Recursive call with new IP
+    return mydig(additional[0].to_text(), domain_name, domain_index-1)
+
+
+def parse_domain(domain_name: str, index: int) -> str:
+    """Parses together specified pieces of the split domain name"""
+    domain_list = domain_name.split(".")
+    domain_list = domain_list[:-1]
+    domain_list = [x + "." for x in domain_list]
+    if index == len(domain_list)-1:
+        return domain_list[len(domain_list)-1]
+
+    parsed = ""
+    while index < len(domain_list):
+        parsed = parsed + domain_list[index]
+        index += 1
+
+    return parsed
 
 
 if __name__ == "__main__":
